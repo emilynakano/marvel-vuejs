b958e0283154f3d72eca08a37a67f4152821b0c4
emily <nakanodepaula@gmail.com>
2023-02-01 10:34:25 -0300

    feat: get all characters in the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getAllCharacters } from "@/services/charactersService";
+
+const characters = ref([]);
+onMounted(async () => {
+  const data = await getAllCharacters(0);
+  characters.value = data;
+});
+</script>
 <template>
   <main>
-    <h1>home</h1>
+    <h1 v-if="characters">{{ characters }}</h1>
   </main>
 </template>
