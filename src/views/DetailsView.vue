2aabd7e2c7b9198ca8d9ce2a16ca958ecca1a021
emily <nakanodepaula@gmail.com>
2023-02-02 09:48:58 -0300

    feat: get id by router param

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import { TrinityRingsSpinner } from "epic-spinners";
+import { useRoute } from "vue-router";
+
+const { params } = useRoute();
+const id = params.id;
+</script>
 <template>
-  <div>details</div>
+  <Header />
+  <main>
+    <div class="spinner">
+      <trinity-rings-spinner
+        :animation-duration="1500"
+        :size="66"
+        color="rgb(197, 12, 12)"
+      />
+    </div>
+  </main>
 </template>
-<style scoped lang="css"></style>
+<style scoped lang="css">
+main {
+  padding: 75px 0 20px 0;
+}
+.spinner {
+  height: calc(100vh - 100px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
