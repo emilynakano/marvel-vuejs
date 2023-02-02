62b7b936b030212493c62e7397615faf646b48ad
emily <nakanodepaula@gmail.com>
2023-02-01 21:26:08 -0300

    feat: character box layout

---
+++ src/components/Characters.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getAllCharacters } from "@/services/charactersService";
+
+interface ICharacter {
+  id: number;
+  name: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+}
+
+const characters = ref<ICharacter[] | []>([]);
+onMounted(async () => {
+  const data = await getAllCharacters(0);
+  characters.value = data.filter(
+    (c: ICharacter) => !c.thumbnail.path.includes("image_not_available")
+  );
+});
+</script>
+<template>
+  <main v-if="characters">
+    <div v-for="character in characters" :key="character.id" class="character">
+      <div class="character">
+        <div class="img-overflow">
+          <img
+            :src="
+              character.thumbnail.path + '.' + character.thumbnail.extension
+            "
+          />
+        </div>
+
+        <div class="row"></div>
+        <div class="content">
+          <h1>{{ character.name.split("(")[0] }}</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<style lang="css">
+main {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+
+.character {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  position: relative;
+
+  width: 160px;
+  background-color: #1b1b1b;
+  border-bottom-right-radius: 20px;
+
+  overflow: hidden;
+}
+
+.row {
+  widows: 100%;
+  height: 4px;
+  background-color: rgb(197, 12, 12);
+}
+
+.img-overflow {
+  overflow: hidden;
+  width: 160px;
+  height: 200px;
+}
+img {
+  width: 160px;
+  height: 200px;
+}
+
+.content {
+  font-size: 15px;
+  padding: 25px 20px;
+}
+
+h1 {
+  font-size: 15px;
+  font-weight: 800;
+  color: white;
+  width: 110px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-transform: uppercase;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { getAllCharacters } from "@/services/charactersService";
-
-interface ICharacter {
-  id: number;
-  name: string;
-  thumbnail: {
-    path: string;
-    extension: string;
-  };
-}
-
-const characters = ref<ICharacter[] | []>([]);
-onMounted(async () => {
-  const data = await getAllCharacters(0);
-  characters.value = data;
-});
+import Characters from "@/components/Characters.vue";
 </script>
 <template>
-  <main v-if="characters">
-    <div v-for="character in characters" :key="character.id" class="character">
-      <div class="character">
-        <h1>{{ character.name }}</h1>
-        <img
-          :src="character.thumbnail.path + '.' + character.thumbnail.extension"
-          alt=""
-        />
-      </div>
-    </div>
-  </main>
+  <Characters />
 </template>
