fb60a3a46d0b348e517b5301e379438fb8584deb
emily <nakanodepaula@gmail.com>
2023-02-03 17:43:21 -0300

    chore: install and config font awesome

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,10 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faMagnifyingGlass);
+
+app.component("font-awesome-icon", FontAwesomeIcon).mount("#app");
